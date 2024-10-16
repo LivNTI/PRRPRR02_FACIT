ce4758394bb3e99c4619e7ed2a81ca441701bd66
liv.sundman <liv.sundman@ga.ntig.se>
2024-10-16 12:50:23 +0200

    Woking on the user interace

--- src/TVSerialization/TvMain.java
+++ src/TVSerialization/TvMain.java
@@ -2,22 +2,62 @@ package TVSerialization;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class TvMain {
     private ArrayList<TvSeries> shows = new ArrayList<>();
+    Scanner myScan = new Scanner(System.in);
 
     public TvMain() {
-        createShowData();
-        write2File();
+        //createShowData();
+        //write2File();
+        readFromFile();
+        startProgram();
 
     }
 
+    private void startProgram() {
+        boolean keepGoing = true;
+        int ans;
+
+        do {
+            System.out.println("you have the following TV shows");
+            int i;
+            for (i = 1; i < shows.size() + 1; i++) {
+                System.out.print(i + ") ");
+                shows.get(i).printInfo();
+            }
+            System.out.println(i++ + ") None, I Quit");
+            System.out.println("Which show do you wnat to work with");
+
+            ans = myScan.nextInt();
+            myScan.nextLine();
+
+            if (ans >= i || ans < 0) {
+
+            }
+
+
+        } while (keepGoing);
+    }
+
     private void createShowData() {
+        Random rand = new Random();
+        String[] showNames = {"Doctor Who", "Stargate: Atlantis", "Castlvania", "One Piece", "CSI"};
+        TvSeries tvShow;
+        for (String name : showNames) {
+            tvShow = new TvSeries(name);
+            shows.add(tvShow);
+            tvShow.changeRating(rand.nextInt(1, 6));
+            tvShow.addEpisodes(rand.nextInt(1, 24), rand.nextInt(1, 6));
+        }
+
     }
 
     public void write2File() {
         try {
-            FileOutputStream fileOUt = new FileOutputStream("./TVSerializationTVShows.ser");
+            FileOutputStream fileOUt = new FileOutputStream("./src/TVSerialization/TVSerializationTVShows.ser");
             ObjectOutputStream objectOut = new ObjectOutputStream(fileOUt);
 
             objectOut.writeObject(shows);
@@ -34,7 +74,7 @@ public class TvMain {
 
     public void readFromFile() {
         try {
-            FileInputStream fileIn = new FileInputStream("./TVSerializationTVShows.ser");
+            FileInputStream fileIn = new FileInputStream("./src/TVSerialization/TVSerializationTVShows.ser");
             ObjectInputStream objectIn = new ObjectInputStream(fileIn);
 
             shows = (ArrayList<TvSeries>) objectIn.readObject();
--- src/TVSerialization/TvSeries.java
+++ src/TVSerialization/TvSeries.java
@@ -1,4 +1,38 @@
 package TVSerialization;
 
-public class TvSeries {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TvSeries implements Serializable {
+    private String name;
+    private ArrayList<Integer> episodes = new ArrayList<>();
+    private int rating;
+
+    public TvSeries() {
+        Scanner myScan = new Scanner(System.in);
+        System.out.print("what is the name of the TV show? ");
+        name = myScan.nextLine();
+    }
+
+    public TvSeries(String inName) {
+        name = inName;
+    }
+
+    public void changeRating(int newRating) {
+        rating = newRating;
+    }
+
+    public void addEpisodes(int numOfEpisodes, int season) {
+        if (season > episodes.size()) {
+            for (int i = 0; i < season - 1; i++) {
+                episodes.add(0);
+            }
+        }
+        episodes.add(season - 1, numOfEpisodes);
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + name + ", Rating: " + rating);
+    }
 }
